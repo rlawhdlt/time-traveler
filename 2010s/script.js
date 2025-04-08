9f6b87adf40585d127a32ff5408c302cc3d42c22
rlawhdlt <rlawhdlt@gmail.com>
2025-04-07 22:26:25 -0400

    Update script.js

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,91 @@
-function toggleContent() {
-	let content = document.querySelector("#content");
-	content.classList.toggle('hidden');
-}
+let videoIDs = [
+	"YmQ7MgPDLNU",
+	"3rmGM5iGtHo",
+	"Nkz4te3liX0",
+	"A38ARSI-wFQ",
+	"9jeJbdVl2jI",
+	"cPJUBQd-PNM",
+	"POmH7dDMDEc",
+	"4UdEFmxRmNE"
+  ];
+  
+  function buildLink(videoId) {
+	return "https://www.youtube.com/embed/" + videoId + "?autoplay=1&mute=1&controls=0&loop=1&playlist=" + videoId;
+  }
+  
+  function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  function makeVideoFrame(videoId) {
+	let frame = document.createElement("div");
+	frame.className = "video-frame";
+  
+	let iframe = document.createElement("iframe");
+	iframe.src = buildLink(videoId);
+	iframe.allow = "autoplay; fullscreen";
+	iframe.setAttribute("allowfullscreen", "");
+  
+	frame.appendChild(iframe);
+  
+	frame.addEventListener("mouseenter", function () {
+	  frame.parentElement.classList.add("paused");
+	});
+  
+	frame.addEventListener("mouseleave", function () {
+	  frame.parentElement.classList.remove("paused");
+	});
+  
+	return frame;
+  }
+  
+  function makeManyFrames(amount) {
+	let group = document.createDocumentFragment();
+  
+	for (let i = 0; i < amount; i++) {
+	  let index = getRandomNumber(0, videoIDs.length - 1);
+	  let id = videoIDs[index];
+	  let one = makeVideoFrame(id);
+	  group.appendChild(one);
+	}
+  
+	return group;
+  }
+  
+  function startScrollLoop(track) {
+	let scrollX = 0;
+	let speed = 0.5;
+  
+	function move() {
+	  if (!track.classList.contains("paused")) {
+		scrollX = scrollX + speed;
+  
+		let limit = track.scrollWidth / 3;
+		if (scrollX >= limit) {
+		  scrollX = 0;
+		}
+  
+		track.style.transform = "translateX(-" + scrollX + "px)";
+	  }
+  
+	  requestAnimationFrame(move);
+	}
+  
+	requestAnimationFrame(move);
+  }
+  
+  function start() {
+	let track = document.getElementById("scroll-track");
+  
+	let i = 0;
+	while (i < 3) {
+	  let group = makeManyFrames(10);
+	  track.appendChild(group);
+	  i = i + 1;
+	}
+  
+	startScrollLoop(track);
+  }
+  
+  window.addEventListener("DOMContentLoaded", start);
+  
